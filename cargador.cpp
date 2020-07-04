97b10ea69faf69bd694bd0d4f747da7fb8ef5f89
trinibucc <tbucciarelli@hotmail.com>
2020-07-04 00:48:55 -0300

    Agregue cargador.cpp con su .h y inexistente.cpp con su .h

---
+++ cargador.cpp
@@ -0,0 +1,82 @@
+
+#include "cargador.h"
+#include <iostream>
+#include "excepcion.h"
+
+Cargador::Cargador() {
+
+    l_peliculas = 0;
+    l_actores = 0;
+}
+
+Cargador::~Cargador() {
+
+
+}
+
+
+void Cargador::cargar_datos(ifstream archivo) {
+
+    string nombre, genero, director, actores;
+    int puntaje;
+    unsigned posicion = 1;
+
+    verificar_existencia(archivo);
+
+    if(archivo.fail())
+        cout << "Error no se pudo abrir el archivo" << endl;
+    while(archivo >> nombre) {
+        archivo >> genero;
+        archivo >> director;
+        archivo >> puntaje;
+        cargar_l_actores(archivo);
+        cargar_l_peliculas(nombre, genero, director, puntaje, posicion);
+        posicion++;
+    }
+    archivo.close();
+
+}
+
+void Cargador::cargar_l_peliculas(string nombre, string genero, string dire, int puntaje, int pos) {
+
+    Pelicula *ptrpelicula = new Pelicula(nombre, genero, puntaje, dire, l_actores);
+    l_peliculas.alta(ptrpelicula, pos);
+
+}
+
+
+void Cargador::cargar_l_actores(ifstream archivo) {
+
+    int contador = 0;
+    string actores;
+    while(getline(archivo, actores, ' ')) {
+        l_actores.alta(actores, contador);
+        contador++;
+    }
+}
+
+
+Lista<Pelicula> Cargador::obtener_l_peliculas() {
+
+    return l_peliculas;
+}
+
+Lista<string> Cargador::obtener_l_actores() {
+
+    return l_actores;
+}
+
+
+void Cargador::verificar_existencia(ifstream nombre_archivo) {
+
+    try {
+
+        if (!nombre_archivo)
+            throw archivo_inexistente;
+    }
+    catch (const exception& e) {
+
+        cout << e.what() << endl;
+    }
+
+}
---
+++ inexistente.h
@@ -0,0 +1,20 @@
+
+
+#ifndef INEXISTENTE_H
+#define INEXISTENTE_H
+
+#include <exception>
+
+using namespace std;
+
+class Archivo_inexistente : public exception{
+
+public:
+    Archivo_inexistente();
+    ~Archivo_inexistente();
+    virtual const char* what() const throw();
+
+} archivo_inexistente;
+
+
+#endif //INEXISTENTE_H
