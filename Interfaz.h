4452908357d9e7985006cfd02aaf23c2a31205ae
esmiroco <67381964+esmiroco@users.noreply.github.com>
2020-07-09 22:04:01 -0300

    Update Interfaz.h

--- Interfaz.h
+++ Interfaz.h
@@ -3,11 +3,14 @@
 
 #include "Lista.h"
 #include "Pelicula.h"
+#include "Cargador.h"
+#include "Recomendaciones.h"
 
 class Interfaz {
 private:
     // almacena la operacion que seleccionará el usuario
     int opcion;
+    Pelicula* puntPeli;
 public:
     //constructor
     //PRE: ninguna
@@ -22,10 +25,10 @@ public:
     //permite seleccionar una opcion
     //PRE: Menu creado
     //POST: propone al usuario una serie de opciones de menu, e inicia las operaciones necesarias para la concrecion de cada una
-    void selec_opcion();
+    void selec_opcion(Caegador carg_vistas, Cargador carg_no_vistas, Recomendadas recomendeichons);
 
     //muestra una lista completa
     //PRE: existencia de una lista
     //POST: a través de un bucle for muestra todos los elementos de una lista, a traves de sus metodos obtener_tamaño y obtener_dato
-    void mostrar();
+    void mostrar(Cargador cargadore);
 }
