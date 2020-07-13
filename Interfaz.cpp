83de3ba0e5cf5d326a9673ac61d2bb9f5a115915
arucattaneo <aru.cattaneo@gmail.com>
2020-07-12 22:53:34 -0300

    formato

--- Interfaz.cpp
+++ Interfaz.cpp
@@ -1,94 +1,75 @@
-
 #include "Interfaz.h"
 #include <iostream>
+
 using namespace std;
 
- Interfaz::Interfaz(Recomendaciones recomendaciones) {
+Interfaz::Interfaz(Recomendaciones recomendaciones) {
     lista_vistas = recomendaciones.obtener_lista_vistas();
     lista_no_vistas = recomendaciones.obtener_lista_no_vistas();
     lista_recomendadas = recomendaciones.obtener_lista_recomendadas();
     opcion = 0;
- }
+}
 
-
-
- void Interfaz::ejecutar_interfaz(){
-
-    do{
+void Interfaz::ejecutar_interfaz() {
+    do {
         mostrar_opciones();
         solicitar_opcion();
         redireccionar_opcion();
 
-    }while (opcion != 4);
+    } while (opcion != 4);
 }
 
 
 void Interfaz::mostrar_opciones() {
-
     cout << "-------------------Menu-------------------" << endl;
     cout << "1.- Mostrar las peliculas vistas" << endl;
     cout << "2.- Mostrar las peliculas no vistas" << endl;
     cout << "3.- Mostrar las peliculas recomendadas" << endl;
     cout << "4.- Salir" << endl;
-
 }
 
 void Interfaz::solicitar_opcion() {
-
     cout << "Elija la accion que desea realizar" << endl;
     cin >> opcion;
 }
 
-
 void Interfaz::redireccionar_opcion() {
-
-	switch(opcion){
-		case 1:{
-			mostrar_peliculas_vistas();
-			break;
-		}
-		case 2:{
-			mostrar_peliculas_no_vistas();
-			break;
-		}
-		case 3:{
-			mostrar_peliculas_recomendadas();
-			break;
-		}
-
-	}
-
- }
-
-
- void Interfaz::mostrar_peliculas_vistas() {
-    Pelicula *pelicula;
-    for(unsigned i = 1; i <= (lista_vistas->obtener_tamanio()); i++) {
-         pelicula = lista_vistas->obtener_dato(i);
-         pelicula->mostrar_pelicula();
+    switch (opcion) {
+        case 1: {
+            mostrar_peliculas_vistas();
+            break;
+        }
+        case 2: {
+            mostrar_peliculas_no_vistas();
+            break;
+        }
+        case 3: {
+            mostrar_peliculas_recomendadas();
+            break;
+        }
     }
-
- }
-
- void Interfaz::mostrar_peliculas_no_vistas() {
-    Pelicula *pelicula;
-    for(unsigned i = 1; i <= (lista_no_vistas->obtener_tamanio()); i++) {
-         pelicula = lista_no_vistas->obtener_dato(i);
-         pelicula->mostrar_pelicula();
-    }
-
- }
-
- void Interfaz::mostrar_peliculas_recomendadas() {
-    Pelicula *pelicula;
-    for(unsigned i = 1; i <= (lista_recomendadas->obtener_tamanio()); i++) {
-         pelicula = lista_recomendadas -> obtener_dato(i);
-         pelicula->mostrar_pelicula();
-    }
-
 }
 
+void Interfaz::mostrar_peliculas_vistas() {
+    Pelicula *pelicula;
+    for (unsigned i = 1; i <= (lista_vistas->obtener_tamanio()); i++) {
+        pelicula = lista_vistas->obtener_dato(i);
+        pelicula->mostrar_pelicula();
+    }
+}
 
-Interfaz::~Interfaz() {
+void Interfaz::mostrar_peliculas_no_vistas() {
+    Pelicula *pelicula;
+    for (unsigned i = 1; i <= (lista_no_vistas->obtener_tamanio()); i++) {
+        pelicula = lista_no_vistas->obtener_dato(i);
+        pelicula->mostrar_pelicula();
+    }
+}
 
+void Interfaz::mostrar_peliculas_recomendadas() {
+    Pelicula *pelicula;
+    for (unsigned i = 1; i <= (lista_recomendadas->obtener_tamanio()); i++) {
+        pelicula = lista_recomendadas->obtener_dato(i);
+        pelicula->mostrar_pelicula();
+    }
 }
--- Interfaz.h
+++ Interfaz.h
@@ -17,30 +17,35 @@ private:
 public:
     //Constructor
     Interfaz(Recomendaciones recomendaciones);
-    //Destructor
-    ~Interfaz();
+
     //PRE: Interfaz debe estar credo
     //POS: Muestra las acciones posible por pantalla y redirige la accion segun la opcion ingresada
     void ejecutar_interfaz();
+
     //PRE:--
     //POS: Muestra por pantalla las acciones posibles
     void mostrar_opciones();
+
     //PRE: Debe recibir un unsigned
     //POS: Solicita por pantalla el ingresa de una opcion
     void solicitar_opcion();
+
     //PRE:--
     //POS: Muestra por pantalla la lista de peliculas vistas
     //PRE:  0 < opcion >= 3
     //POS: Segun el valor del atributo opcion realiza la accion correspondiente
     void redireccionar_opcion();
+
     void mostrar_peliculas_vistas();
+
     //PRE:--
     //POS: Muestra por pantalla la lista de peliculas no vistas
     void mostrar_peliculas_no_vistas();
+
     //PRE:--
     //POS: Muestra por pantalla la lista de peliculas recomendadas
     void mostrar_peliculas_recomendadas();
-    
+
 };
 
 #endif //INTERFAZ_H
