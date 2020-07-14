ec0d1824c21c36032a7dd00cdf4fdd4dcf3216ee
esmiroco <67381964+esmiroco@users.noreply.github.com>
2020-07-13 23:07:29 -0300

    Update Interfaz.h

--- Interfaz.h
+++ Interfaz.h
@@ -17,36 +17,35 @@ private:
 public:
     //Constructor
     Interfaz(Recomendaciones recomendaciones);
-
-    //PRE: Interfaz debe estar credo
-    //POS: Muestra las acciones posible por pantalla y redirige la accion segun la opcion ingresada
+    //PRE: Ninguna
+    //POS: Muestra las acciones posibles por pantalla y redirige la accion segun la opcion ingresada
     void ejecutar_interfaz();
 
-    //PRE:--
+    //PRE: Objeto interfaz creado
     //POS: Muestra por pantalla las acciones posibles
     void mostrar_opciones();
 
-    //PRE: Debe recibir un unsigned
-    //POS: Solicita por pantalla el ingresa de una opcion
+    //PRE: Objeto interfaz creado
+    //POS: Solicita por pantalla el ingreso de una opcion
     void solicitar_opcion();
 
     //PRE:  0 < opcion >= 3
     //POS: Segun el valor del atributo opcion realiza la accion correspondiente
     void redireccionar_opcion();
 
-    //PRE:--
+    //PRE: Ejecucion del metodo redireccionar_opcion
     //POS: Muestra por pantalla la lista de peliculas vistas
     void mostrar_peliculas_vistas();
 
-    //PRE:--
+    //PRE: Ejecucion del metodo redireccionar_opcion
     //POS: Muestra por pantalla la lista de peliculas no vistas
     void mostrar_peliculas_no_vistas();
 
-    //PRE:--
+    //PRE: Ejecucion del metodo redireccionar_opcion
     //POS: Muestra por pantalla la lista de peliculas recomendadas
     void mostrar_peliculas_recomendadas();
 
-    //PRE: --
+    //PRE: lista peliculas creada
     //POS: Borrar los elementos de una lista pertenecientes al puntero a lista pasado por parametro
     void borrar_lista(Lista<Pelicula *> *lista_aux);
 
